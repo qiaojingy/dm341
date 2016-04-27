0ba6fcb186061305646ed2e5eac27061b99d27d4
Wei Wang <wwang23@stanford.edu>
2016-04-26 20:49:07 -0700

    n2 finished

--- java/src/dm341/score/Classify.java
+++ java/src/dm341/score/Classify.java
@@ -4,27 +4,73 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import dm341.util.DistanceMeasure;
+import dm341.util.Pair;
+
 public class Classify {
-	private HashMap<String, ArrayList<String>> aliases;
+	private HashMap<String, Pair<Integer, HashSet<String>>> aliases;
+	
+	public static double THRESHOLD = 0.6;
+	public static int KGram = 3;	
 	
 	public Classify() {
-		aliases = new HashMap<String, ArrayList<String>>();
+		aliases = new HashMap<String, Pair<Integer, HashSet<String>>>();
 	}
 	
-	public HashMap<String, ArrayList<String>> mergeOrganizations(ArrayList<FCCRecord> records) {
-		HashMap<String, HashSet<String>> ads = new HashMap<String, HashSet<String>>();
+	public HashSet<String> getAliases(String organization) {
+		if (!aliases.containsKey(organization)) {
+			System.err.println("Bad Call!");
+		}
+		return aliases.get(organization).getSecond();
+	}
+	
+	public HashMap<String, HashSet<String>> mergeOrganizations(ArrayList<FCCRecord> records) {
+		HashMap<String, Pair<Integer, HashSet<String>>> ads = new HashMap<String, Pair<Integer, HashSet<String>>> ();
+		// merge same 
 		for (FCCRecord record: records) {
 			String organization = record.getOrgName();
 			String stationID = record.getStationID();
 			if (!ads.containsKey(organization)) {
 				HashSet<String> stations = new HashSet<String>();
 				stations.add(stationID);
-				ads.put(organization, stations);
+				ads.put(organization, new Pair<Integer, HashSet<String>> (1, stations));
 			} else {
-				ads.get(organization).add(stationID);
+				Pair<Integer, HashSet<String>> myPair = ads.get(organization);
+				myPair.setFirst(myPair.getFirst() + 1);
+				myPair.getSecond().add(stationID);
 			}
 		}
-	}
-	
-	
+		
+		// merge similar
+		HashMap<String, HashSet<String>> uniqueOrgAds = new HashMap<String, HashSet<String>>();
+		HashSet<String> mergedOrgs = new HashSet<String> ();
+		
+		for (String currOrgName : ads.keySet()) {
+			if (mergedOrgs.contains(currOrgName)) continue;
+			
+			HashSet<String> stationIds = new HashSet<String> ();
+			stationIds.addAll(ads.get(currOrgName).getSecond());
+			String currKey = currOrgName;
+			
+			for (String otherOrgName : ads.keySet()) {
+				if (currOrgName.equals(otherOrgName) || currKey.equals(otherOrgName)) continue;
+				if (DistanceMeasure.JaccardSimilarity(KGram, currKey, otherOrgName) >= THRESHOLD) {
+					int otherCount = ads.get(otherOrgName).getFirst();
+					if (ads.get(currKey).getFirst() >= otherCount) {
+						aliases.get(currKey).getSecond().add(otherOrgName);
+					} else {
+						aliases.put(otherOrgName, aliases.get(currKey));
+						aliases.get(otherOrgName).setFirst(otherCount);
+						aliases.get(otherOrgName).getSecond().add(currKey);
+						aliases.remove(currKey);
+					}
+					mergedOrgs.add(otherOrgName);
+					stationIds.addAll(ads.get(otherOrgName).getSecond());
+				} 
+			}
+			
+			uniqueOrgAds.put(currKey, stationIds);
+		}
+		return uniqueOrgAds;
+	}	
 }
