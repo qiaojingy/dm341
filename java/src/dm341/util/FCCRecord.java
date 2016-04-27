14de89d22aecd06434556f23c559f7214e3cf624
qiaojingy <qiaojingyan9@gmail.com>
2016-04-26 20:59:57 -0700

    commit

--- java/src/dm341/util/FCCRecord.java
+++ java/src/dm341/util/FCCRecord.java
@@ -4,4 +4,23 @@ public class FCCRecord {
 	String stationID;
 	String orgName;
 	String url;
+	
+	public FCCRecord(String stationID, String url) {
+		this.stationID = stationID;
+		String orgName = url.split("/")[-2];
+		this.orgName = orgName;
+		this.url = url;
+	}
+	
+	public String getStationID() {
+		return stationID;
+	}
+	
+	public String getOrgName() {
+		return orgName;
+	}
+	
+	public String url() {
+		return url;
+	}
 }
--- java/src/dm341/util/IO.java
+++ java/src/dm341/util/IO.java
@@ -5,10 +5,14 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class IO {
 	static String data_path;
+	static boolean initialized = false;
 	static void initialize() throws IOException {
 		/* Read configuration file */
 		String config_path = "./configure.txt";
@@ -19,23 +23,62 @@ public class IO {
 		}
 		BufferedReader reader = new BufferedReader(new FileReader(config));
 		data_path = reader.readLine();
+		System.out.println("here" + data_path);
 		reader.close();
 	}
 	static List<FCCRecord> readFCCRecords() throws IOException {
+		if (!initialized)
+		    initialize();
 		/* Get index directory */
-		String input_path = data_path + "/FCC/docs.txt";
-		BufferedReader adRecordReader = new BufferedReader(new FileReader(new File(
-				input_path)));
+		String input_path = data_path + "/FCC/test.txt";
+		BufferedReader adRecordReader = new BufferedReader(new InputStreamReader(new FileInputStream(input_path),
+		        "UTF8"));
 		String line;
 		line = adRecordReader.readLine();
-		System.out.println(line);
-		/***
-		while ((line = adRecordReader.readLine()) != null) {
-			String[] tokens = line.split("\t");
-			termDict.put(tokens[0], Integer.parseInt(tokens[1]));
+		adRecordReader.readLine();
+		String fieldsLine = line.substring(2);
+		fieldsLine = line.replace("\0", "");
+		System.out.println((int) fieldsLine.charAt(0));
+		System.out.println((int) fieldsLine.charAt(1));
+		System.out.println((int) fieldsLine.charAt(2));
+		System.out.println((int) fieldsLine.charAt(3));
+		System.out.println((int) fieldsLine.charAt(4));
+		System.out.println((int) fieldsLine.charAt(5));
+		System.out.println((int) fieldsLine.charAt(6));
+		System.out.println((int) fieldsLine.charAt(7));
+		System.out.println((int) fieldsLine.charAt(8));
+		System.out.println((int) fieldsLine.charAt(9));
+
+		String[] fields = fieldsLine.split("|");
+		Map<String, Integer> fieldsDict = new HashMap<String, Integer>();
+		for (int i = 0; i < fields.length; i++) {
+			fieldsDict.put(fields[i], i);
 		}
-		termReader.close();
-		***/
+		List<FCCRecord> FCCRecords = new ArrayList<FCCRecord>();
+		StringBuilder sb = new StringBuilder();
+		while ((line = adRecordReader.readLine()) != null) {
+			if (line.length() == 1 && line.charAt(0) == 0) {
+				System.out.println("**********");
+				String s = sb.toString();
+				String[] datum = s.split("|");
+				/***
+				for (int i = 0; i < datum.length; i++) {
+					System.out.println(datum[i]);
+				}
+				***/
+				FCCRecords.add(new FCCRecord(datum[fieldsDict.get("station_id")], datum[fieldsDict.get("url")]));
+				sb.setLength(0);
+			} else {
+				sb.append(line);
+			}
+		}
+		adRecordReader.close();
+		for (FCCRecord fr : FCCRecords) {
+			System.out.println(fr.getOrgName());
+		}
 		return null;
+	}
+	public static void main(String[] args) throws IOException {
+		readFCCRecords();
 	}
 }
